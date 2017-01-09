af699adf5d96aab052811a1726f3d558dfafd680
jpmmaia <jpmmaia@gmail.com>
2017-01-09 19:11:34 +0100

    - Moving toolbox. - Implementing BuildVocabulary.

--- Assignment 5/Source/main.m
+++ Assignment 5/Source/main.m
@@ -1,3 +1,8 @@
+run('..\..\vlfeat-0.9.20\toolbox\vl_setup.m');
+
+folder = '../Resources/train/';
+clusterCount = 50;
+
 vocabulary = BuildVocabulary(folder, clusterCount);
 [training, group] = BuildKNN(folder, vocabulary);
 confusionMatrix = ClassifyImages(folder, vocabulary, training, group);
