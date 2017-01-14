b39a7dee33ec56f3bffe8e8b5064005f7735a4e1
dagcan <mermi.dagcan@gmail.com>
2017-01-14 14:41:01 +0100

    Visualize descriptor matches

--- Assignment 4/Source/SIFT_Point_Detection.m
+++ Assignment 4/Source/SIFT_Point_Detection.m
@@ -2,12 +2,22 @@ function SIFT_Point_Detection
 
 baseInputPath = '../Resources/';
 
-image = im2single(rgb2gray(imread(strcat(baseInputPath,'campus2.jpg'))));
+image1 = im2single(rgb2gray(imread(strcat(baseInputPath,'campus2.jpg'))));
+image2 = im2single(rgb2gray(imread(strcat(baseInputPath,'campus3.jpg'))));
 
-imshow(image);
+imshow(image1);
 
-F = vl_sift(image);
-vl_plotframe(F);
+[fa, da] = vl_sift(image1) ;
+[fb, db] = vl_sift(image2) ;
+
+
+[matches, scores] = vl_ubcmatch(da,db);
+
+matches1 = fa(1:2,matches(1,1:60));
+matches2 = fb(1:2,matches(2,1:60));
+match_plot(image1,image2,matches1',matches2');
+
+%vl_plotframe(F1);
 
 
 end
